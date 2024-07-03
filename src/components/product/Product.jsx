1b42649ee980f91c71dbc2e27d130763940cc668
Mуhammadsaid <muhammadsaidmm4@gmail.com>
2024-07-03 22:17:03 +0300

    appdat

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsCart, BsCartCheck } from "react-icons/bs";
 import { FiHeart } from "react-icons/fi";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { Link, useNavigate } from "react-router-dom";
-import Category from "../category/Category";
 import Skeleton from "../skeleton/Skeleton";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../context/slices/cartSlice";
 import { toggleHeart } from "../../context/slices/wishlistSlice";
-import Model from "../model/Model";
+import { useGetCategoryQuery } from "../../context/api/categoryApi";
+// import Model from "../model/Model";
 
 const Product = ({ isMarriade, data, isLoading, justifyContent }) => {
   const wishlist = useSelector((state) => state?.wishlist?.value);
@@ -17,7 +17,19 @@ const Product = ({ isMarriade, data, isLoading, justifyContent }) => {
   const dispatch = useDispatch();
   let navigate = useNavigate();
 
-  let productItems = data?.map((el) => (
+  const { data: categories } = useGetCategoryQuery();
+  const [categoryValue, setCategoryValue] = useState("all");
+  const [filteredData, setFilteredData] = useState(null);
+
+  useEffect(() => {
+    if (categoryValue === "all") {
+      setFilteredData(data);
+    } else {
+      setFilteredData(data?.filter((i) => i.category === categoryValue));
+    }
+  }, [categoryValue, data]);
+
+  let productItems = filteredData?.map((el) => (
     <div key={el.id} className="product__card">
       <img
         onClick={() => setToggle(!toggle)}
@@ -62,28 +74,49 @@ const Product = ({ isMarriade, data, isLoading, justifyContent }) => {
     </div>
   ));
 
+  let categoryItems = categories?.map((el) => (
+    <li
+      className={`category__item ${categoryValue === el.title ? "click" : ""}`}
+      key={el.id}
+    >
+      <span onClick={() => setCategoryValue(el.title)}>{el.title}</span>
+    </li>
+  ));
+
   return (
     <div>
       <section className="product">
         <div className="container">
           <div className="home__titles">
             <h1 className="home__titles__title">Популярные товары</h1>
-            {isMarriade ? (
+            {isMarriade && (
               <button
                 onClick={() => navigate("/all-products")}
                 className="home__titles__btn display__none"
               >
                 Все товары <IoIosArrowRoundForward />
               </button>
-            ) : (
-              <></>
             )}
           </div>
-          {isMarriade ? <Category products={data} /> : <></>}
+          {isMarriade ? (
+            <ul className="category__list">
+              <li
+                className={`category__item ${
+                  categoryValue === "all" ? "click" : ""
+                }`}
+                value={"all"}
+              >
+                <span onClick={() => setCategoryValue("all")}>All</span>
+              </li>
+              {categoryItems}
+            </ul>
+          ) : (
+            <></>
+          )}
           <div style={{ justifyContent }} className="product__cards">
             {isLoading ? <Skeleton /> : productItems}
           </div>
-          {isMarriade ? (
+          {isMarriade && (
             <button
               onClick={() => navigate("/all-products")}
               style={{ display: "none" }}
@@ -91,8 +124,6 @@ const Product = ({ isMarriade, data, isLoading, justifyContent }) => {
             >
               Весь товары <IoIosArrowRoundForward />
             </button>
-          ) : (
-            <></>
           )}
         </div>
 
